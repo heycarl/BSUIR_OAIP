214eadab5be797fb1d4a8888b6899d897543699d
Alexandr Efimchik <efimchiksasha@gmail.com>
2021-11-22 13:08:34 +0300

    LAB6_1 optimization

--- LAB6/6_1.c
+++ LAB6/6_1.c
@@ -11,29 +11,22 @@ int main()
     int n = 0;
     scanf("%d", &n);
 
-    int *ptr = (int *)malloc(n * sizeof(int));
+    int *ptr = (int*)malloc(n * sizeof(int));
     for (int i = 0; i < n; i++)
     {
-        scanf("%d", ptr + i);
+        scanf("%d", &ptr[i]);
     }
-    int *A[5];
-    A[0] = ptr + 0;
-    A[1] = ptr + 1;
-    A[2] = ptr + 2;
-    A[3] = ptr + 3;
-    A[4] = ptr + 4;
+
     if (!ptr)
         return 0;
     for (int i = 0; i < n; i++)
     {
-        int a = *(ptr + i);
-        int b = *(ptr + i + 1);
-        if (a < b)
+        if (ptr[i] < ptr[i+1])
         {
             printf("%d is smaller than %d \n", i, i+1);
             for (int j = i; j < n-1; j++)
             {
-                *(ptr + j) = *(ptr + j + 1);
+                ptr[j] = ptr[j+1];
             }
             n--;
             if (realloc(ptr, n * sizeof(int)))
@@ -44,7 +37,7 @@ int main()
     }
     for (int i = 0; i < n; i++)
     {
-        printf("%d ", *(ptr + i));
+        printf("%d ", ptr[i]);
     }
     printf("\n");
     return 0;
